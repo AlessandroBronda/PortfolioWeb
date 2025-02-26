e37764b7755c8ff426ad6c8ae9578fecf1f067c9
Alessandro <alessandrobronda@gmail.com>
2025-02-26 13:23:00 +0100

    v2.2

--- portfolio/src/Home.js
+++ portfolio/src/Home.js
@@ -1,14 +1,23 @@
+import React, { useState } from "react";
 import "./Home.css";
+import "./media.css";
 const semoyImage = process.env.PUBLIC_URL + "/semoy.jpg";
 
 function App() {
+	const [menuOpen, setMenuOpen] = useState(false); // Stato per aprire/chiudere la navbar
+
 	return (
 		<div className="App">
 			<header className="App-header">
+				{/* Bottone Hamburger per mobile */}
+				<button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+					☰
+				</button>
+
 				<div className="LeftCl">
 					<img className="profilePic" src={semoyImage} alt="Semoy" />
 					<h1 className="Name">
-						Bronda <br></br> Alessandro
+						Bronda <br /> Alessandro
 					</h1>
 					<p className="bio">
 						<i>
@@ -17,28 +26,27 @@ function App() {
 						</i>
 					</p>
 				</div>
+
 				<div className="RightCl">
-					What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting
-					industry. Lorem Ipsum has been the industry's standard dummy text ever since the
-					1500s, when an unknown printer took a galley of type and scrambled it to make a type
-					specimen book. It has survived not only five centuries, but also the leap into
-					electronic typesetting, remaining essentially unchanged. It was popularised in the
-					1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
-					recently with desktop publishing software like Aldus PageMaker including versions of
-					Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be
-					distracted by the readable content of a page when looking at its layout. The point of
-					using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as
-					opposed to using 'Content here, content here', making it look like readable English.
-					Many desktop publishing packages and web page editors now use Lorem Ipsum as their
-					default model text, and a search for 'lorem ipsum' will uncover many web sites still
-					in their infancy. Various versions have evolved over the years, sometimes by
-					accident, sometimes on purpose (injected humour and the like).
+					<ul className={`sections ${menuOpen ? "open" : ""}`}>
+						<li>HOME</li>
+						<li>BIO</li>
+						<li>PROGETTI3D</li>
+						<li>ANIMAZIONE</li>
+						<li>GRAFICA</li>
+						<li>PROGRAMMAZIONE</li>
+						<li>WEBDESIGN</li>
+					</ul>
+
+					<div className="cMaster">
+						<div className="c1"></div>
+						<div className="c2"></div>
+						<div className="c3"></div>
+					</div>
 				</div>
 			</header>
 		</div>
 	);
 }
-
-
 
 export default App;
