6f846e69a0c9fcfb532e609ecd8a6d4769183310
Alessandro <alessandrobronda@gmail.com>
2025-03-02 11:42:00 +0100

    v2.5

--- portfolio/src/1_Home/Home.js
+++ portfolio/src/1_Home/Home.js
@@ -7,14 +7,14 @@ const semoyImage = process.env.PUBLIC_URL + "/semoy.jpg";
 function App() {
 	const [menuOpen, setMenuOpen] = useState(false);
 	const [isMobileNav, setIsMobileNav] = useState(
-		window.innerWidth >= 700 && window.innerWidth < 1300
+		window.innerWidth >= 800 && window.innerWidth < 1450
 	);
 
 	// Controlla il ridimensionamento della finestra
 	useEffect(() => {
 		const handleResize = () => {
 			const width = window.innerWidth;
-			setIsMobileNav(width >= 700 && width < 1300);
+			setIsMobileNav(width >= 800 && width < 1450);
 		};
 
 		window.addEventListener("resize", handleResize);
@@ -23,7 +23,7 @@ function App() {
 
 	return (
 		<div className="App">
-			{/* Sposta il menu in cima solo tra 700px e 1300px */}
+			{/* Sposta il menu in cima solo tra 800px e 1450px */}
 			{isMobileNav && (
 				<nav id="mainMenu" className={`sections ${menuOpen ? "open" : ""}`}>
 					<div className="navbar">
@@ -72,10 +72,13 @@ function App() {
 					<div className="cMaster">
 						<div className="c1"></div>
 						<div className="c2"></div>
-						<div className="c3"></div>
+						{window.innerWidth < 800 && <div className="c3"></div>}{" "}
+						{/* Dentro SOLO se < 800px */}
 					</div>
+					{window.innerWidth > 1450 && <div className="c3 specialPosition"></div>}
 				</div>
 			</header>
+			{window.innerWidth >= 800 && window.innerWidth <= 1450 && <div className="c3"></div>}
 		</div>
 	);
 }
